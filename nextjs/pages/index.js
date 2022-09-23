108cb3073aa59bea605f199e1c24284d04fa293a
indicozy <berkutkarlibai@gmail.com>
2022-09-24 05:15:34 +0600

    test

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Heading } from '@chakra-ui/react'
-
+import Head from "next/head";
+import Image from "next/image";
+import { Heading } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <div>
-      <Heading>I'm a Heading</Heading>
+      <Heading>I&apos;m a Heading</Heading>
     </div>
-  )
+  );
 }
