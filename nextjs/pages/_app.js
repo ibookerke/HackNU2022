6ba8bda9262dda2103a4073fbecd40a5d385900f
indicozy <berkutkarlibai@gmail.com>
2022-09-24 05:21:30 +0600

    test

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,11 +1,21 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
-
-export default MyApp;
