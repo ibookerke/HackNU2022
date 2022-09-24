9c8dcb52a20a582323a39e57c5cf1e8f42ab58c3
indicozy <berkutkarlibai@gmail.com>
2022-09-24 16:35:57 +0600

    map

--- nextjs/pages/index.jsx
+++ nextjs/pages/index.jsx
@@ -1,11 +1,13 @@
 // import Head from "next/head";
 // import Image from "next/image";
 import { Heading } from "@chakra-ui/react";
+import Map from '../components/map'
 
 export default function Home() {
   return (
     <div>
       <Heading>I&apos;m a Heading</Heading>
+      <Map/>
     </div>
   );
 }
