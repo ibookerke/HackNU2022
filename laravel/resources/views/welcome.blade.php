b3f5e00f1f5f54a887ef36a296385a7d917c2936
ibookerke <ibookaitkozha@gmail.com>
2022-09-25 05:40:46 +0600

    interface + filters realised

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -14,13 +14,8 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-
         @stack('css')
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
+
     </head>
     <body style="width: 100vw; height: 100vh">
 
