f90de800de5804751a9870def088522182a018fd
ibookerke <ibookaitkozha@gmail.com>
2022-09-25 06:28:52 +0600

    lala

--- laravel/resources/views/components/main_modal.blade.php
+++ laravel/resources/views/components/main_modal.blade.php
@@ -20,30 +20,30 @@
                         <div class="card-body">
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">From</span>
-                                <input type="date" class="form-control" aria-label="date from" aria-describedby="basic-addon1">
+                                <input type="date" class="form-control" aria-label="date from" aria-describedby="basic-addon1" onchange="filterChangeFromDate(event)">
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">To &nbsp; &nbsp; </span>
-                                <input type="date" class="form-control" aria-label="date from" aria-describedby="basic-addon1">
+                                <input type="date" class="form-control" aria-label="date from" aria-describedby="basic-addon1" onchange="filterChangeToData(event)">
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Age</span>
-                                <select class="form-select">
-                                    <option value="">12-20</option>
-                                    <option value="">20-40</option>
-                                    <option value="">40+</option>
+                                <select class="form-select" id="filterAgeInput" onchange="filterAgeChange()">
+                                    <option value="1">12-20</option>
+                                    <option value="2">20-40</option>
+                                    <option value="3">40+</option>
                                 </select>
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Gender</span>
-                                <select class="form-select">
-                                    <option value="">Male</option>
-                                    <option value="">Female</option>
-                                    <option value="">Other</option>
+                                <select class="form-select" id="filterGenderInput" onchange="filterGenderChange()">
+                                    <option value="1">Male</option>
+                                    <option value="2">Female</option>
+                                    <option value="3">Other</option>
                                 </select>
                             </div>
 
-                            <button type="button" class="btn btn-primary btn">Apply</button>
+                            <button type="button" class="btn btn-primary btn" onclick="applyFilters()">Apply</button>
                         </div>
                     </div>
 
@@ -87,6 +87,31 @@
 
 @push('scripts')
     <script defer>
+        var filterObject = {
+            from: null,
+            to: null,
+            age: null,
+            gender: null
+        }
 
+        function filterChangeFromDate(e) {
+            filterObject.from = e.target.value;
+        }
+
+        function filterChangeToData(e) {
+            filterObject.to = e.target.value
+        }
+
+        function filterAgeChange() {
+            filterObject.age = document.getElementById('filterAgeInput').value
+        }
+
+        function filterGenderChange() {
+            filterObject.gender = document.getElementById('filterGenderInput').value
+        }
+
+        function applyFilters() {
+            console.log(filterObject)
+        }
     </script>
 @endpush
